9df4dd1df1902eaebd32627ee9f44b1b538335a9
Daniel Ron <edaniel.ron@outlook.com>
2019-06-09 03:27:01 -0500

    Change response to NOT_FOUND instead of BAD_REQUEST

--- src/main/java/com/example/rmmservices/exception/CustomerNotFoundException.java
+++ src/main/java/com/example/rmmservices/exception/CustomerNotFoundException.java
@@ -3,6 +3,6 @@ package com.example.rmmservices.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Customer not found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Customer not found")
 public class CustomerNotFoundException extends RuntimeException {
 }
--- src/main/java/com/example/rmmservices/exception/DeviceNotFoundException.java
+++ src/main/java/com/example/rmmservices/exception/DeviceNotFoundException.java
@@ -3,6 +3,6 @@ package com.example.rmmservices.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Device not found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Device not found")
 public class DeviceNotFoundException extends RuntimeException {
 }
